7102c072923e0b415ec70dd84beacc861c9847d1
dominicbeesley <dominic@authorityfile.co.uk>
2022-07-11 17:32:23 +0100

    line-1 hardware test move to new registers (not checked)

--- hardware-testing/blit-line/line-1/line-1.asm
+++ hardware-testing/blit-line/line-1/line-1.asm
@@ -10,47 +10,55 @@
 		lda	#12
 		jsr	OSWRCH
 
+		; enable device and set page
+		lda 	#$D1
+		sta 	zp_mos_jimdevsave
+		sta 	fred_JIM_DEVNO
+
+		ldx 	#jim_page_DMAC
+		stx 	fred_JIM_PAGE_HI
+
 
 		; line drawing test
 		;============================
 		; set start point address
 		lda	#$FF
-		sta	sheila_DMAC_ADDR_C
-		sta	sheila_DMAC_ADDR_D
+		sta	jim_DMAC_ADDR_C
+		sta	jim_DMAC_ADDR_D
 		ldx	#$5600
-		stx	sheila_DMAC_ADDR_C+1
-		stx	sheila_DMAC_ADDR_D+1
+		stx	jim_DMAC_ADDR_C+1
+		stx	jim_DMAC_ADDR_D+1
 		ldx	#640
-		stx	sheila_DMAC_STRIDE_C
-		stx	sheila_DMAC_STRIDE_D
+		stx	jim_DMAC_STRIDE_C
+		stx	jim_DMAC_STRIDE_D
 
 		; set start point pixel mask and colour
 		lda	#$0F
-		sta	sheila_DMAC_DATA_B		; colour 1bpp white
+		sta	jim_DMAC_DATA_B			; colour 1bpp white
 		lda	#$11				; 8bpp left middle pixel
-		sta	sheila_DMAC_DATA_A
+		sta	jim_DMAC_DATA_A
 		; set major length
 		ldx	#100
-		stx	sheila_DMAC_WIDTH		; 16 bits!
+		stx	jim_DMAC_WIDTH			; 16 bits!
 		; set slope
 		ldd	#30				; major length
-		std	sheila_DMAC_ADDR_B+1
+		std	jim_DMAC_ADDR_B+1
 		lsra
 		rorb
-		std	sheila_DMAC_ADDR_A+1		; initial error accumulator value
+		std	jim_DMAC_ADDR_A+1			; initial error accumulator value
 		ldd	#10
-		std	sheila_DMAC_STRIDE_A
+		std	jim_DMAC_STRIDE_A
 
 		;set func gen to be plot B masked by A
 		lda	#$CA				; B masked by A
-		sta	sheila_DMAC_FUNCGEN
+		sta	jim_DMAC_FUNCGEN
 
 		; set bltcon 0
 		lda	#BLITCON_EXEC_C + BLITCON_EXEC_D
-		sta	sheila_DMAC_BLITCON
+		sta	jim_DMAC_BLITCON
 		; set bltcon 1 - right/down
 		lda	#BLITCON_ACT_ACT + BLITCON_ACT_CELL + BLITCON_ACT_LINE
-		sta	sheila_DMAC_BLITCON
+		sta	jim_DMAC_BLITCON
 
 		nop
 		nop
